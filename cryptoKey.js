88bd01a22e81ef88cd26e07897f9559e84287493
Aniket <goreaniket100@gmail.com>
2025-05-19 20:57:23 +0530

    node js project ccomming

--- cryptoKey.js
+++
@@ -1,6 +0,0 @@
-const crypto = require('crypto');
-
-const pass = crypto.pbkdf2('password', 'salt', 1, 64, 'sha512', (err, derivedKey) => {
-  if (err) throw err;
-  console.log(derivedKey.toString('hex')); // '3745e48...a0b9'
-}   );
